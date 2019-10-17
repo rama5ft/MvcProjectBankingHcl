bcc285940723051da483d2f0e38a3c687663ec60
Muppidi Ramadevi <ramadevi.muppidi2@outlook.com>
2019-10-17 12:26:26 +0530

    MvcBankingProjectHcl is created and added two classes related account 17/10/2019 at 12:26pm

---
+++ MvcProjectBankingHcl/Models/AccountDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjectBankingHcl.Models
+{
+    public class AccountDetails
+    {
+        public string AccountDetailsId { get; set; }
+        public string AccountHolderName { get; set; }
+        public string AccountNumber { get; set; }
+        public string IFSCCode{ get; set; }
+        public string BankBranch { get; set; }
+    }
+}
---
+++ MvcProjectBankingHcl/Models/AccountHolderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjectBankingHcl.Models
+{
+    public class AccountHolderDetails
+    {
+        public int AccountHolderDetailsid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string  MobileNumber { get; set; }
+        public string AlternateMobileNumber { get; set; }
+        public string EmailId { get; set; }
+        public int MyProperty { get; set; }
+        public List<string> Nationality { get; set; }
+        public string Address { get; set; }
+        public byte[] IdProofs { get; set; }
+        public string Status { get; set; }
+    }
+}
